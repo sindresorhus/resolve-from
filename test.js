9f0f315a7efc9f19220a821a8196936b215288ec
Sam Verschueren <sam.verschueren@gmail.com>
2017-04-27 16:56:41 +0200

    Tweak error messages

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ import test from 'ava';
 import m from './';
 
 test(t => {
+	t.throws(() => m(1, './fixture'), 'Expected `fromDir` to be of type `string`, got `number`');
+	t.throws(() => m('fixture'), 'Expected `moduleId` to be of type `string`, got `undefined`');
 	t.regex(m('fixture', './fixture'), /fixture\/fixture\.js$/);
 	t.is(m('fixture', './fixture2'), null);
 });
