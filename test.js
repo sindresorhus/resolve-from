33c68bd9bf450a3f66158f61ab57ab1222a28b73
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-13 20:10:55 +0200

    Require Node.js 8, return `undefined` instead of `null`, add TypeScript definition

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 import test from 'ava';
-import m from '.';
+import resolveFrom from '.';
 
 test('resolveFrom()', t => {
-	t.throws(() => m(1, './fixture'), /got `number`/);
-	t.throws(() => m('fixture'), /got `undefined`/);
+	t.throws(() => resolveFrom(1, './fixture'), /got `number`/);
+	t.throws(() => resolveFrom('fixture'), /got `undefined`/);
 
-	t.regex(m('fixture', './fixture'), /fixture\/fixture\.js$/);
+	t.regex(resolveFrom('fixture', './fixture'), /fixture\/fixture\.js$/);
 
 	const moduleNotFoundError = t.throws(() => {
-		m('fixture', './nonexistent');
+		resolveFrom('fixture', './nonexistent');
 	}, Error);
 	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
 	t.is(moduleNotFoundError.message, 'Cannot find module \'./nonexistent\'');
 
-	const resolveFromfixture = m.bind(null, 'fixture');
+	const resolveFromfixture = resolveFrom.bind(null, 'fixture');
 	t.regex(resolveFromfixture('./fixture'), /fixture\/fixture\.js$/);
 
-	t.truthy(m('./fixture/fixture-for-symlinks/symlink-target', 'foo'));
+	t.truthy(resolveFrom('./fixture/fixture-for-symlinks/symlink-target', 'foo'));
 });
 
 test('resolveFrom.silent()', t => {
-	t.regex(m.silent('fixture', './fixture'), /fixture\/fixture\.js$/);
-	t.is(m.silent('fixture', './nonexistent'), null);
+	t.regex(resolveFrom.silent('fixture', './fixture'), /fixture\/fixture\.js$/);
+	t.is(resolveFrom.silent('fixture', './nonexistent'), undefined);
 
-	const silentResolveFromfixture = m.silent.bind(null, 'fixture');
+	const silentResolveFromfixture = resolveFrom.silent.bind(null, 'fixture');
 	t.regex(silentResolveFromfixture('./fixture'), /fixture\/fixture\.js$/);
 
-	t.is(m.silent('fixture-not-exists', './fixture'), null);
+	t.is(resolveFrom.silent('fixture-not-exists', './fixture'), undefined);
 });
